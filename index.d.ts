5b00e8a5277888fa56e45e6f1acb63987718759b
Ankkiyy <ankit932753@gmail.com>
2024-10-30 21:55:09 +0530

    Added TS to the Proejct

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+// node-ms-uia/index.d.ts
+declare module 'node-ms-uia' {
+    export function printMsg(): void; // Declare the function and its return type
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// node-ms-uia/index.js
+export function printMsg() {
+    console.log("Hello from node-ms-uia");
+  }
